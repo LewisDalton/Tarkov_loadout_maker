00e7759c6334c93a416d1d999d096b1ba28c5474
LewisDalton <139710354+LewisDalton@users.noreply.github.com>
2024-11-17 15:21:57 +0000

    remove unnecessary js

--- script.js
+++ script.js
@@ -5,12 +5,3 @@ document
     const img_element = document.getElementById("helmet_image");
     img_element.src = selected_value;
   });
-
-const div = document.getElementById("character");
-const img = new Image(); // Create an off-screen image element
-
-img.src = "assets/images/char_background.png"; // Replace with the actual image path
-img.onload = function () {
-  div.style.width = `${img.naturalWidth}px`; // Set div width to the image width
-  div.style.height = `${img.naturalHeight}px`; // Set div height to the image height
-};

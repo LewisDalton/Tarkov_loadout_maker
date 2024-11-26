d5e26717364a714d126678713e2b0e69e76c5df3
Lewis Dalton <139710354+LewisDalton@users.noreply.github.com>
2024-11-26 17:31:56 +0000

    Learning to use the tarkov.dev graphql api

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ document
     const img_element = document.getElementById("helmet_image");
     img_element.src = selected_value;
   });
+
+fetch("https://api.tarkov.dev/graphql", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    query: `{
+    itemsByType(type: helmet) {
+        name
+        shortName
+        iconLink
+    }
+}`,
+  }),
+})
+  .then((r) => r.json())
+  .then((data) => console.log("data returned:", data));

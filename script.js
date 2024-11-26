984e9b091eb248ce4ba1f276451f66b7310fa04c
LewisDalton <139710354+LewisDalton@users.noreply.github.com>
2024-11-26 22:40:11 +0000

    Started working on listening for clicking on all selects, then calling the required data for that select so that it can populate the select list with item choices.

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// Captures all of the elements taht have the square_container or rec_container class
+// And then stores their ids in an array
+const elements = document.querySelectorAll(".square_container, .rec_container");
+const ids = Array.from(elements).map((element) => element.id);
+console.log(ids);
+
+for (let i = 0; i < ids.length; i++) {
+  console.log(ids[i]);
+}
+
+// Fetches all helmet items when the select element is clicked.
+document.getElementById("helmet_select").addEventListener("click", function () {
+  fetch("https://api.tarkov.dev/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      query: `{
+    itemsByType(type: helmet) {
+        name
+        shortName
+        iconLink
+    }
+}`,
+    }),
+  })
+    .then((r) => r.json())
+    .then((data) => console.log("data returned:", data));
+});
+
 document
   .getElementById("helmet_select")
   .addEventListener("change", function () {
@@ -5,22 +37,3 @@ document
     const img_element = document.getElementById("helmet_image");
     img_element.src = selected_value;
   });
-
-fetch("https://api.tarkov.dev/graphql", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-  body: JSON.stringify({
-    query: `{
-    itemsByType(type: helmet) {
-        name
-        shortName
-        iconLink
-    }
-}`,
-  }),
-})
-  .then((r) => r.json())
-  .then((data) => console.log("data returned:", data));
